0f1bfb37139ab67a1603032caea346e32364dbab
Dominik Richter <dominik.richter@gmail.com>
2014-05-07 01:34:36 +0200

    added full sysctl configuration
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- manifests/sysctl.pp
+++ manifests/sysctl.pp
@@ -9,13 +9,34 @@
 class os_hardening::sysctl (
   $enable_module_loading = true,
   $cpu_vendor = "intel",
-  $desktop_enabled = false
+  $desktop_enabled = false,
+  $enable_ipv4_forwarding = false,
+  $enable_ipv6 = false,
+  $enable_ipv6_forwarding = false,
+  $arp_restricted = true,
+  $enable_sysrq = false,
+  $enable_core_dump = false,
 ){
   # set variables
   if $architecture == "amd64" or $architecture == "x86_64" {
     $x86_64 = true
   } else {
     $x86_64 = false
+  }
+
+  # configure sysctl parameters
+  file { '/etc/sysctl.conf':
+    content => template('os_hardening/sysctl.conf.erb'),
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => 644,
+    notify  => Exec['reload_sysctl'],
+  }
+
+  exec { '/sbin/sysctl -p':
+    alias       => 'reload_sysctl',
+    refreshonly => true,
   }
 
   # configure for module hardening
