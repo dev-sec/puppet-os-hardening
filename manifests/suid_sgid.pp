b639897bd4a80ebfe5adbe18aecd92ba0cc2e55a
Dominik Richter <dominik.richter@gmail.com>
2014-05-19 16:16:18 +0200

    puppet-lint suid_sgid
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- manifests/suid_sgid.pp
+++ manifests/suid_sgid.pp
@@ -16,93 +16,108 @@ class os_hardening::suid_sgid (
   # suid and sgid blacklists and whitelists
   # ---------------------------------------
   # don't change values in the system_blacklist/whitelist
-  # adjust values for blacklist/whitelist instead, they can override system_blacklist/whitelist
+  # adjust values for blacklist/whitelist instead,
+  # they can override system_blacklist/whitelist
 
   # list of suid/sgid entries that must be removed
   $system_blacklist = [
   # blacklist as provided by NSA
-  "/usr/bin/rcp", "/usr/bin/rlogin", "/usr/bin/rsh",
+  '/usr/bin/rcp', '/usr/bin/rlogin', '/usr/bin/rsh',
   # sshd must not use host-based authentication (see ssh cookbook)
-  "/usr/libexec/openssh/ssh-keysign",
-  "/usr/lib/openssh/ssh-keysign",
+  '/usr/libexec/openssh/ssh-keysign',
+  '/usr/lib/openssh/ssh-keysign',
   # misc others
-  "/sbin/netreport",                                            # not normally required for user
-  "/usr/sbin/usernetctl",                                       # modify interfaces via functional accounts
+  # not normally required for user
+  '/sbin/netreport',
+  # modify interfaces via functional accounts
+  '/usr/sbin/usernetctl',
   # connecting to ...
-  "/usr/sbin/userisdnctl",                                      # no isdn...
-  "/usr/sbin/pppd",                                             # no ppp / dsl ...
+  # no isdn...
+  '/usr/sbin/userisdnctl',
+  # no ppp / dsl ...
+  '/usr/sbin/pppd',
   # lockfile
-  "/usr/bin/lockfile",
-  "/usr/bin/mail-lock",
-  "/usr/bin/mail-unlock",
-  "/usr/bin/mail-touchlock",
-  "/usr/bin/dotlockfile",
+  '/usr/bin/lockfile',
+  '/usr/bin/mail-lock',
+  '/usr/bin/mail-unlock',
+  '/usr/bin/mail-touchlock',
+  '/usr/bin/dotlockfile',
   # need more investigation, blacklist for now
-  "/usr/bin/arping",
-  "/usr/sbin/uuidd",
-  "/usr/bin/mtr",                                               # investigate current state...
-  "/usr/lib/evolution/camel-lock-helper-1.2",                   # investigate current state...
-  "/usr/lib/pt_chown",                                          # pseudo-tty, needed?
-  "/usr/lib/eject/dmcrypt-get-device",
-  "/usr/lib/mc/cons.saver"                                      # midnight commander screensaver
+  '/usr/bin/arping',
+  '/usr/sbin/uuidd',
+  # investigate current state...
+  '/usr/bin/mtr',
+  # investigate current state...
+  '/usr/lib/evolution/camel-lock-helper-1.2',
+  # pseudo-tty, needed?
+  '/usr/lib/pt_chown',
+  '/usr/lib/eject/dmcrypt-get-device',
+  # midnight commander screensaver
+  '/usr/lib/mc/cons.saver',
   ]
 
   # list of suid/sgid entries that can remain untouched
   $system_whitelist = [
   # whitelist as provided by NSA
-  "/bin/mount", "/bin/ping", "/bin/su", "/bin/umount", "/sbin/pam_timestamp_check",
-  "/sbin/unix_chkpwd", "/usr/bin/at", "/usr/bin/gpasswd", "/usr/bin/locate",
-  "/usr/bin/newgrp", "/usr/bin/passwd", "/usr/bin/ssh-agent", "/usr/libexec/utempter/utempter", "/usr/sbin/lockdev",
-  "/usr/sbin/sendmail.sendmail", "/usr/bin/expiry",
+  '/bin/mount', '/bin/ping', '/bin/su', '/bin/umount',
+  '/sbin/pam_timestamp_check','/sbin/unix_chkpwd', '/usr/bin/at',
+  '/usr/bin/gpasswd', '/usr/bin/locate', '/usr/bin/newgrp',
+  '/usr/bin/passwd', '/usr/bin/ssh-agent', '/usr/libexec/utempter/utempter',
+  '/usr/sbin/lockdev', '/usr/sbin/sendmail.sendmail', '/usr/bin/expiry',
   # whitelist ipv6
-  "/bin/ping6","/usr/bin/traceroute6.iputils",
+  '/bin/ping6','/usr/bin/traceroute6.iputils',
   # whitelist nfs
-  "/sbin/mount.nfs", "/sbin/umount.nfs",
+  '/sbin/mount.nfs', '/sbin/umount.nfs',
   # whitelist nfs4
-  "/sbin/mount.nfs4", "/sbin/umount.nfs4",
+  '/sbin/mount.nfs4', '/sbin/umount.nfs4',
   # whitelist cron
-  "/usr/bin/crontab",
+  '/usr/bin/crontab',
   # whitelist consolemssaging
-  "/usr/bin/wall", "/usr/bin/write",
+  '/usr/bin/wall', '/usr/bin/write',
   # whitelist: only SGID with utmp group for multi-session access
   #            impact is limited; installation/usage has some remaining risk
-  "/usr/bin/screen",
+  '/usr/bin/screen',
   # whitelist locate
-  "/usr/bin/mlocate",
+  '/usr/bin/mlocate',
   # whitelist usermanagement
-  "/usr/bin/chage", "/usr/bin/chfn", "/usr/bin/chsh",
+  '/usr/bin/chage', '/usr/bin/chfn', '/usr/bin/chsh',
   # whitelist fuse
-  "/bin/fusermount",
+  '/bin/fusermount',
   # whitelist pkexec
-  "/usr/bin/pkexec",
+  '/usr/bin/pkexec',
   # whitelist sudo
-  "/usr/bin/sudo","/usr/bin/sudoedit",
+  '/usr/bin/sudo','/usr/bin/sudoedit',
   # whitelist postfix
-  "/usr/sbin/postdrop","/usr/sbin/postqueue",
+  '/usr/sbin/postdrop','/usr/sbin/postqueue',
   # whitelist apache
-  "/usr/sbin/suexec",
+  '/usr/sbin/suexec',
   # whitelist squid
-  "/usr/lib/squid/ncsa_auth", "/usr/lib/squid/pam_auth",
+  '/usr/lib/squid/ncsa_auth', '/usr/lib/squid/pam_auth',
   # whitelist kerberos
-  "/usr/kerberos/bin/ksu",
+  '/usr/kerberos/bin/ksu',
   # whitelist pam_caching
-  "/usr/sbin/ccreds_validate",
+  '/usr/sbin/ccreds_validate',
   # whitelist Xorg
-  "/usr/bin/Xorg",                                              # xorg
-  "/usr/bin/X",                                                 # xorg
-  "/usr/lib/dbus-1.0/dbus-daemon-launch-helper",                # freedesktop ipc
-  "/usr/lib/vte/gnome-pty-helper",                              # gnome
-  "/usr/lib/libvte9/gnome-pty-helper",                          # gnome
-  "/usr/lib/libvte-2.90-9/gnome-pty-helper"                     # gnome
+  '/usr/bin/Xorg',
+  '/usr/bin/X',
+  # freedesktop ipc
+  '/usr/lib/dbus-1.0/dbus-daemon-launch-helper',
+  # gnome
+  '/usr/lib/vte/gnome-pty-helper',
+  '/usr/lib/libvte9/gnome-pty-helper',
+  '/usr/lib/libvte-2.90-9/gnome-pty-helper',
   ]
 
-  $final_blacklist = combine_sugid_lists($system_blacklist, $whitelist, $blacklist)
-  $final_whitelist = combine_sugid_lists($system_whitelist, $blacklist, $whitelist)
+  $final_blacklist = combine_sugid_lists(
+    $system_blacklist, $whitelist, $blacklist)
+  $final_whitelist = combine_sugid_lists(
+    $system_whitelist, $blacklist, $whitelist)
 
+  # Remove SUID and SGID bits from a given file
   define blacklistFiles {
-    exec{ "remove suid/sgid bit from $name":
-      command => "/bin/chmod ug-s $name",
-      onlyif => "/usr/bin/test -f $name",
+    exec{ "remove suid/sgid bit from ${name}":
+      command => "/bin/chmod ug-s ${name}",
+      onlyif  => "/usr/bin/test -f ${name}",
     }
   }
 
@@ -112,10 +127,10 @@ class os_hardening::suid_sgid (
     # create a helper script
     # TODO: do without
     file { '/usr/local/sbin/remove_suids':
-      ensure => file,
-      owner  => root,
-      group  => root,
-      mode   => 500,
+      ensure  => file,
+      owner   => root,
+      group   => root,
+      mode    => '0500',
       content => template('os_hardening/remove_sugid_bits.erb'),
     }
     ->
