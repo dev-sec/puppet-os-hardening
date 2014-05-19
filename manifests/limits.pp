6a1de86166d8537be77582485e64cc3a9871dedd
Dominik Richter <dominik.richter@gmail.com>
2014-05-19 16:47:46 +0200

    puppet-lint limits
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- manifests/limits.pp
+++ manifests/limits.pp
@@ -1,7 +1,7 @@
 # == Class: os_hardening::limits
 #
 # Configures the limits.conf. Sets:
-# 
+#
 # * disable core dumps
 #
 # === Copyright
@@ -12,13 +12,12 @@ class os_hardening::limits (
   $allow_core_dumps = false
 ){
   if $allow_core_dumps == false {
-    file {
-      '/etc/security/limits.d/10.hardcore.conf':
+    file { '/etc/security/limits.d/10.hardcore.conf':
         ensure => present,
         source => 'puppet:///modules/os_hardening/limits.conf',
-        owner => root,
-        group => root,
-        mode => 400
+        owner  => root,
+        group  => root,
+        mode   => '0400',
     }
   }
 }
