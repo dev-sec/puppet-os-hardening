4dabdb7cc68bf360ff4405f7d4ded2f100b2e072
Dominik Richter <dominik.richter@gmail.com>
2014-05-19 16:32:56 +0200

    puppet-lint securetty
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- manifests/securetty.pp
+++ manifests/securetty.pp
@@ -7,14 +7,14 @@
 # Copyright 2014, Deutsche Telekom AG
 #
 class os_hardening::securetty (
-  $root_ttys = ["console","tty1","tty2","tty3","tty4","tty5","tty6"]
+  $root_ttys = ['console','tty1','tty2','tty3','tty4','tty5','tty6']
 ){
-  $ttys = join( $root_ttys, "\n")
+  $ttys = join( $root_ttys, '\n')
   file { '/etc/securetty':
-    ensure => present,
+    ensure  => present,
     content => template( 'os_hardening/securetty.erb' ),
-    owner => root,
-    group => root,
-    mode => 400
+    owner   => root,
+    group   => root,
+    mode    => '0400',
   }
 }
