170fadf905ea5e097084bcefe3810dbf4c038413
bitvijays <bitvijays@gmail.com>
2017-05-23 00:43:17 +0530

    Puppet-lint done for sysctl.pp Part 2

--- manifests/sysctl.pp
+++ manifests/sysctl.pp
@@ -67,7 +67,8 @@ class os_hardening::sysctl (
   sysctl { 'net.ipv6.conf.all.accept_ra': value => '0' }
   sysctl { 'net.ipv6.conf.default.accept_ra': value => '0' }
 
-  # Enable RFC-recommended source validation feature. It should not be used for routers on complex networks, but is helpful for end hosts and routers serving small networks.
+  # Enable RFC-recommended source validation feature. It should not be used for routers on complex networks, but is helpful for 
+  # end hosts and routers serving small networks.
   sysctl { 'net.ipv4.conf.all.rp_filter': value => '1' }
   sysctl { 'net.ipv4.conf.default.rp_filter': value => '1' }
 
@@ -80,7 +81,8 @@ class os_hardening::sysctl (
   # Limit the amount of traffic the system uses for ICMP.
   sysctl { 'net.ipv4.icmp_ratelimit': value => '100' }
 
-  # Adjust the ICMP ratelimit to include: ping, dst unreachable, source quench, time exceed, param problem, timestamp reply, information reply
+  # Adjust the ICMP ratelimit to include: ping, dst unreachable, source quench, time exceed, param problem, timestamp reply, 
+  # information reply
   sysctl { 'net.ipv4.icmp_ratemask': value => '88089' }
 
   # Protect against wrapping sequence numbers at gigabit speeds:
@@ -90,8 +92,16 @@ class os_hardening::sysctl (
   # Define different restriction levels for announcing the local source IP address from IP packets in ARP requests sent on interface:
   #
   # * **0** - (default) Use any local address, configured on any interface
-  # * **1** - Try to avoid local addresses that are not in the target's subnet for this interface. This mode is useful when target hosts reachable via this interface require the source IP address in ARP requests to be part of their logical network configured on the receiving interface. When we generate the request we will check all our subnets that include the target IP and will preserve the source address if it is from such subnet. If there is no such subnet we select source address according to the rules for level 2.
-  # * **2** - Always use the best local address for this target. In this mode we ignore the source address in the IP packet and try to select local address that we prefer for talks with the target host. Such local address is selected by looking for primary IP addresses on all our subnets on the outgoing interface that include the target IP address. If no suitable local address is found we select the first local address we have on the outgoing interface or on all other interfaces, with the hope we will receive reply for our request and even sometimes no matter the source IP address we announce.
+  # * **1** - Try to avoid local addresses that are not in the target's subnet for this interface. This mode is useful when target 
+  # hosts reachable via this interface require the source IP address in ARP requests to be part of their logical network configured 
+  # on the receiving interface. When we generate the request we will check all our subnets that include the target IP and will 
+  # preserve the source address if it is from such subnet. If there is no such subnet we select source address according to the rules 
+  # for level 2.
+  # * **2** - Always use the best local address for this target. In this mode we ignore the source address in the IP packet and try 
+  # to select local address that we prefer for talks with the target host. Such local address is selected by looking for primary IP 
+  # addresses on all our subnets on the outgoing interface that include the target IP address. If no suitable local address is found 
+  # we select the first local address we have on the outgoing interface or on all other interfaces, with the hope we will receive 
+  # reply for our request and even sometimes no matter the source IP address we announce.
   if $arp_restricted {
     sysctl { 'net.ipv4.conf.all.arp_ignore': value => '1' }
   } else {
@@ -103,7 +113,8 @@ class os_hardening::sysctl (
   #
   # * **0** - (default): reply for any local target IP address, configured on any interface
   # * **1** - reply only if the target IP address is local address configured on the incoming interface
-  # * **2** - reply only if the target IP address is local address configured on the incoming interface and both with the sender's IP address are part from same subnet on this interface
+  # * **2** - reply only if the target IP address is local address configured on the incoming interface and both with the sender's 
+  # IP address are part from same subnet on this interface
   # * **3** - do not reply for local addresses configured with scope host, only resolutions for global and link addresses are replied
   # * **4-7** - reserved
   # * **8** - do not reply for all local addresses
@@ -153,7 +164,8 @@ class os_hardening::sysctl (
   }
   #kernel.modules_disabled = <%= @enable_module_loading ? 0 : 1 %>
 
-  # Magic Sysrq should be disabled, but can also be set to a safe value if so desired for physical machines. It can allow a safe reboot if the system hangs and is a 'cleaner' alternative to hitting the reset button.
+  # Magic Sysrq should be disabled, but can also be set to a safe value if so desired for physical machines. It can allow a safe reboot if 
+  # the system hangs and is a 'cleaner' alternative to hitting the reset button.
   # The following values are permitted:
   #
   # * **0** - disable sysrq
