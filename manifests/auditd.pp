2be8147e6f9b8cdf3af60d2c4e80130fe2753855
Tim Stoop <tim@kumina.nl>
2018-06-11 13:17:43 +0200

    Fix service and dependencies.
    HardeningFramework-DCO-1.1-Signed-off-by: Tim Stoop <github@timstoop.nl> (github: timstoop)

--- manifests/auditd.pp
+++ manifests/auditd.pp
@@ -20,35 +20,40 @@ class os_hardening::auditd (
   }
 
   service { 'auditd':
-    ensure  => running,
-    enabled => true,
+    ensure => running,
+    enable => true,
   }
 
   file_line { 'CIS DIL Benchmark 4.1.1.1 - Ensure audit log storage size is configured':
-    path  => '/etc/audit/auditd.conf',
-    match => '^max_log_file =.*',
-    line  => "max_log_file = ${max_log_file}",
+    path   => '/etc/audit/auditd.conf',
+    match  => '^max_log_file =.*',
+    line   => "max_log_file = ${max_log_file}",
+    notify => Service['auditd'],
   }
 
   file_line {
     'CIS DIL Benchmark 4.1.1.2 - Ensure system is disabled when audit logs are full, space_left_action':
-      path  => '/etc/audit/auditd.conf',
-      match => '^space_left_action =.*',
-      line  => 'space_left_action = email';
+      path   => '/etc/audit/auditd.conf',
+      match  => '^space_left_action =.*',
+      notify => Service['auditd'],
+      line   => 'space_left_action = email';
     'CIS DIL Benchmark 4.1.1.2 - Ensure system is disabled when audit logs are full, action_mail_acct':
-      path  => '/etc/audit/auditd.conf',
-      match => '^action_mail_acct =.*',
-      line  => 'action_mail_acct = root';
+      path   => '/etc/audit/auditd.conf',
+      match  => '^action_mail_acct =.*',
+      notify => Service['auditd'],
+      line   => 'action_mail_acct = root';
     'CIS DIL Benchmark 4.1.1.2 - Ensure system is disabled when audit logs are full, admin_space_left_action':
-      path  => '/etc/audit/auditd.conf',
-      match => '^admin_space_left_action =.*',
-      line  => 'admin_space_left_action = halt';
+      path   => '/etc/audit/auditd.conf',
+      match  => '^admin_space_left_action =.*',
+      notify => Service['auditd'],
+      line   => 'admin_space_left_action = halt';
   }
 
   file_line { 'CIS DIL Benchmark 4.1.1.3 - Ensure audit logs are not automatically deleted':
     path  => '/etc/audit/auditd.conf',
     match => '^max_log_file_action =.*',
     line  => "max_log_file_action = ${max_log_file_action}",
+    notify => Service['auditd'],
   }
 
   if $max_log_file_action == 'rotate' {
@@ -56,6 +61,7 @@ class os_hardening::auditd (
       path  => '/etc/audit/auditd.conf',
       match => '^num_logs =.*',
       line  => "num_logs = ${num_logs}",
+      notify => Service['auditd'],
     }
   }
 
@@ -63,6 +69,7 @@ class os_hardening::auditd (
   exec { 'CIS DIL Benchmark 4.1.3 - Ensure auditing for processes that start prior to auditd is enabled':
     command => "/bin/sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT=\"\\(.*\\)\"/GRUB_CMDLINE_LINUX_DEFAULT=\"\\1  net.ifnames=0 biosdevname=0\"/g' /etc/default/grub;",
     unless  => "/bin/grep GRUB_CMDLINE_LINUX /etc/default/grub | /bin/grep -q 'audit=1'",
+    notify => Service['auditd'],
   }
 
 }
