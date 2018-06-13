21a53bf56eb00dc100459d9e0a05e1aa92d84e02
Tim Stoop <tim@kumina.nl>
2018-06-13 21:23:35 +0200

    Actually add the file.
    HardeningFramework-DCO-1.1-Signed-off-by: Tim Stoop <github@timstoop.nl> (github: timstoop)

---
+++ manifests/services.pp
@@ -0,0 +1,39 @@
+# === Copyright
+#
+# Copyright 2018, Kumina B.V., Tim Stoop
+# Licensed under the Apache License, Version 2.0 (the "License");
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+
+# == Class: os_hardening::services
+#
+# Configures specific services that do not require a full class of their own
+#
+class os_hardening::services (
+  Array   $unwanted_packages = ['telnet'],
+  Array   $wanted_packages   = ['ntp'],
+  Array   $disabled_services = ['rsync'],
+) {
+  # Remove packages that should not be installed
+  $unwanted_packages.each |String $package| {
+    package { $package:
+      ensure => purged,
+    }
+  }
+
+  # Add packages that should be installed
+  $wanted_packages.each |String $package| {
+    package { $package:
+      ensure => installed,
+    }
+  }
+
+  # Disable services that are not wanted
+  $disabled_services.each |String $service| {
+    service { $service:
+      enable => false,
+    }
+  }
+
+}
+
