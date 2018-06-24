4919aa76a74e49821e25ca65ee99995ecbca843f
Tim Stoop <tim@kumina.nl>
2018-06-24 14:21:37 +0200

    Prevent command from executing when apparmor is not set up.
    HardeningFramework-DCO-1.1-Signed-off-by: Tim Stoop <github@timstoop.nl> (github: timstoop)

--- manifests/apparmor.pp
+++ manifests/apparmor.pp
@@ -35,7 +35,7 @@ class os_hardening::apparmor (
   if $enforce_all {
     exec { 'Enforce all AppArmor profiles on the system':
       command => '/usr/sbin/aa-enforce /etc/apparmor.d/*',
-      onlyif  => '/bin/sh -c "[ $(apparmor_status --enforced) -lt $(apparmor_status --profiled) ]"',
+      onlyif  => ['/bin/sh -c "[ $(apparmor_status --enforced) -lt $(apparmor_status --profiled) ]"', '/usr/sbin/apparmor_status >/dev/null 2>&1; retval=$?; [ $retval -eq 0 ] || [ $retval -eq 2 ]'],
     }
   }
 
