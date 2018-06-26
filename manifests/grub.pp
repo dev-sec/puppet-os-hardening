756230cc20bbefdc4a5addbc15c17e70bf5ddaab
Tim Stoop <tim@kumina.nl>
2018-06-26 09:15:09 +0200

    Add code to allow the system to boot without password.
    Now only a password is required when someone wants to change the boot
    parameters. Normal boot is allowed without password.
    
    HardeningFramework-DCO-1.1-Signed-off-by: Tim Stoop <github@timstoop.nl> (github: timstoop)

--- manifests/grub.pp
+++ manifests/grub.pp
@@ -10,13 +10,23 @@
 # Hardens the grub config
 #
 class os_hardening::grub (
-  String  $user          = 'root',
-  String  $password_hash = false,
+  String  $user                  = 'root',
+  String  $password_hash         = false,
+  Boolean $boot_without_password = true,
 ) {
 
   file { '/etc/grub.d/01_hardening':
     content => template('os_hardening/grub_hardening.erb'),
     notify  => Exec['Grub configuration recreate for os_hardening::grub'],
+  }
+
+  if $boot_without_password {
+    # This sets up Grub on Debian Stretch so you can still boot the system without a password
+    exec { 'Keep system bootable without credentials':
+      command => "/bin/sed -i -e 's/^CLASS=\"\\(.*\\)\"/CLASS=\"\\1 --unrestricted\"/' /etc/grub.d/10_linux;",
+      unless  => '/bin/grep -e "^CLASS=" /etc/grub.d/10_linux | /bin/grep -q "--unrestricted"',
+      notify  => Exec['Grub configuration recreate for os_hardening::grub'],
+    }
   }
 
   exec { 'Grub configuration recreate for os_hardening::grub':
