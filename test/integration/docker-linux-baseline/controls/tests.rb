f79293e1eacca37be97824d407b480804fb60df3
Michael Geiger <info@mgeiger.de>
2019-01-18 15:22:54 +0100

    Add kitchen test for OpenSUSE
    Signed-off-by: Michael Geiger <info@mgeiger.de>

--- test/integration/docker-linux-baseline/controls/tests.rb
+++ test/integration/docker-linux-baseline/controls/tests.rb
@@ -1,4 +1,6 @@
 include_controls 'linux-baseline' do
+  # /etc/hosts.equiv is not absent everywhere
+  skip_control 'os-01'
   # skip entropy test, as our short living test VMs usually do not have enough
   skip_control 'os-08'
   # skip filesystem test (vfat enabled for uefi)
@@ -8,7 +10,7 @@ include_controls 'linux-baseline' do
   # skip auditd tests, we do not have any implementation for audit management yet
   skip_control 'package-08'
 
-  # skip all sysctl tests
+  # docker environment - skip all sysctl tests
   skip_control 'sysctl-01'
   skip_control 'sysctl-02'
   skip_control 'sysctl-03'
