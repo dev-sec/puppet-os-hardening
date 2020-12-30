f68b7bb92a7d7e96a3b1fc7601fb4435cbd422fe
Martin Schurz <Martin.Schurz@t-systems.com>
2020-12-30 14:48:39 +0100

    use new syntax for stub in rspec
    Signed-off-by: Martin Schurz <Martin.Schurz@t-systems.com>

--- spec/facter/retrieve_system_users_spec.rb
+++ spec/facter/retrieve_system_users_spec.rb
@@ -4,14 +4,14 @@ require 'spec_helper'
 describe 'retrieve_system_users', type: :fact do
   before(:each) do
     Facter.clear
-    File.stubs(:readline).with('/etc/login.defs')
-        .returns(IO.read("#{File.dirname(__FILE__)}/../fixtures/retrieve_system_users/login.defs"))
+    allow(File.to receive(:readline).with('/etc/login.defs')
+        .and_return(IO.read("#{File.dirname(__FILE__)}/../fixtures/etc/login.defs"))
 
     user1 = Puppet::Type.type(:user).new(name: 'root', ensure: 'present')
     user2 = Puppet::Type.type(:user).new(name: 'bin', ensure: 'present')
     user3 = Puppet::Type.type(:user).new(name: 'daemon', ensure: 'present')
 
-    Puppet::Type.type(:user).stubs(:instances).returns([user1, user2, user3])
+    allow(Puppet::Type.type(:user)).to receive(:instances).and_return([user1, user2, user3])
   end
   after(:each) { Facter.clear }
   context 'on CentOS is should' do
