ba7e3d860c18dcc0eaa9663010cb2d7d0ff70a21
Michael Geiger <info@mgeiger.de>
2021-07-09 09:54:11 +0200

    update to PDK template 2.1.1 (#278)
    Signed-off-by: Michael Geiger <info@mgeiger.de>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,6 +47,18 @@ RSpec.configure do |c|
   c.filter_run_excluding(bolt: true) unless ENV['GEM_BOLT']
   c.after(:suite) do
   end
+
+  # Filter backtrace noise
+  backtrace_exclusion_patterns = [
+    %r{spec_helper},
+    %r{gems},
+  ]
+
+  if c.respond_to?(:backtrace_exclusion_patterns)
+    c.backtrace_exclusion_patterns = backtrace_exclusion_patterns
+  elsif c.respond_to?(:backtrace_clean_patterns)
+    c.backtrace_clean_patterns = backtrace_exclusion_patterns
+  end
 end
 
 # Ensures that a module is defined
